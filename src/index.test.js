7fabce885041799c017cb50383b8c2ff9a23d8ae
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-29 22:40:09 -0500

    ...
    didn't get much further...

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,20 @@
 //mocha does not come with an assertion library, so we are going to use Chai
 import {expect} from "chai"//named reference
+import jsdom from "jsdom";
+import fs from "fs";
 
 describe("Our first test",() => {
     it('should pass', () => {
         expect(true).to.equal(true);
     });
+});
+
+describe("index.html", () => {
+    it("should say hello", () => {
+        const index = fs.readFileSync("./src/index.html", "utf-8");
+        const {JSDOM} = jsdom;
+        const dom = new JSDOM(index);
+        const h1 = dom.window.document.getElementsByTagName("h1")[0];
+        expect(h1.innerHTML).to.equal("Hello World");
+    })
 });
